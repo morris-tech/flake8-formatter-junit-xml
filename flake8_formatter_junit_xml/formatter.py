6aec6ab48f68d2a76eb128ba6eb146f2adbef9b1
Trent Holliday <tholliday@morristechnology.com>
2020-02-19 10:48:32 -0500

    Removed show_statistics and show_benchmark output from file.

--- flake8_formatter_junit_xml/formatter.py
+++ flake8_formatter_junit_xml/formatter.py
@@ -5,6 +5,33 @@ from junit_xml import TestSuite, TestCase
 class JUnitXmlFormatter(base.BaseFormatter):
     """JUnit XML formatter for Flake8."""
 
+    # Override show_statistics to always print to stdout instead of to the file
+    def show_statistics(self, statistics):
+        for error_code in statistics.error_codes():
+            stats_for_error_code = statistics.statistics_for(error_code)
+            statistic = next(stats_for_error_code)
+            count = statistic.count
+            count += sum(stat.count for stat in stats_for_error_code)
+            print(
+                "{count:<5} {error_code} {message}".format(
+                    count=count,
+                    error_code=error_code,
+                    message=statistic.message,
+                ),
+                end=self.newline
+            )
+
+    # Override show_benchmarks to always print to stdout instead of to the file.
+    def show_benchmarks(self, benchmarks):
+        float_format = "{value:<10.3} {statistic}".format
+        int_format = "{value:<10} {statistic}".format
+        for statistic, value in benchmarks:
+            if isinstance(value, int):
+                benchmark = int_format(statistic=statistic, value=value)
+            else:
+                benchmark = float_format(statistic=statistic, value=value)
+            print(benchmark, end=self.newline)
+
     def after_init(self):
         self.test_suites = {}
 
@@ -22,7 +49,7 @@ class JUnitXmlFormatter(base.BaseFormatter):
         name = '{0}, {1}'.format(error.code, error.text)
         test_case = TestCase(
             name,
-            classname=error.filename,
+            # classname=error.filename,
             file=error.filename,
             line=error.line_number
         )
@@ -41,7 +68,7 @@ class JUnitXmlFormatter(base.BaseFormatter):
     # Add a dummy test if no error found
     def finished(self, filename):
         if len(self.test_suites[filename].test_cases) == 0:
-            dummy_case = TestCase("Check passed", classname=filename, file=filename)
+            dummy_case = TestCase("Check passed", file=filename)
             self.test_suites[filename].test_cases.append(dummy_case)
 
     # sort to generate a stable output
